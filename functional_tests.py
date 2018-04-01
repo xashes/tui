102f87b70470d4b9c6ea36fe60224f52ee1200be
kellerx <keller0319@gmail.com>
2018-04-01 16:37:28 +0800

    FT for homepage title display

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,16 @@
-from selenium import webdriver
+import pytest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# Keller has heard about a online market charts app. He goes to checkout
+# its homepage
+@pytest.fixture(scope='module')
+def browser():
+    from selenium import webdriver
+    browser = webdriver.Firefox()
+    browser.get('http://localhost:8000')
+    return browser
+
+def test_can_input_symbol_and_display_charts(browser):
+    # He notices the page title and header mention "charts"
+    assert 'Charts' in browser.title
+    pytest.fail('Finish the test.')
