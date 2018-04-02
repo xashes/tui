826c5c1474d6774ef34ba3ca722bbfb1041ffff0
kellerx <keller0319@gmail.com>
2018-04-02 18:23:51 +0800

    receive user input

--- functional_tests.py
+++ functional_tests.py
@@ -1,16 +1,41 @@
-import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_input_symbol_and_display_kline(self):
+        # Keller has heard about a online market charts app. He goes to checkout
+        # its homepage
+        self.browser.get('http://localhost:8000/charts/kline')
+
+        # He notices the page title and header mention "Kline"
+        self.assertIn('Kline', self.browser.title)
+
+        # He is invited to enter a symbol straight away.
+        inputbox = self.browser.find_element_by_id('id_symbol')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a symbol'
+        )
+
+        # He types "000001.SH" into a text box
+        sample_symbol = '000001.SH'
+        inputbox.send_keys(sample_symbol)
+
+        # When she hits enter, the page updates, and now the page display a kline
+        # chart with the symbol displayed at the top.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.fail('Finish the test.')
 
 
-# Keller has heard about a online market charts app. He goes to checkout
-# its homepage
-@pytest.fixture(scope='module')
-def browser():
-    from selenium import webdriver
-    browser = webdriver.Firefox()
-    browser.get('http://localhost:8000/charts/kline')
-    return browser
-
-def test_can_input_symbol_and_display_charts(browser):
-    # He notices the page title and header mention "charts"
-    assert 'Kline' in browser.title
-    pytest.fail('Finish the test.')
+if __name__ == '__main__':
+    unittest.main()
