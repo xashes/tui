0c30d6e4c9fd8a25284f5076c1257b13ce5e71fb
kellerx <keller0319@gmail.com>
2018-04-08 22:59:38 +0800

    add kline template

--- charts/views.py
+++ charts/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def kline(request):
-    return HttpResponse("<html><title>Kline</title></html>")
+    return render(request, 'kline.html')
--- functional_tests/test_kline.py
+++ functional_tests/test_kline.py
@@ -1,9 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+
+
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -11,31 +14,22 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_input_symbol_and_display_kline(self):
-        # Keller has heard about a online market charts app. He goes to checkout
+        # Helen has heard about a online market charts app. She goes to checkout
         # its homepage
         self.browser.get('http://localhost:8000/charts/kline')
 
-        # He notices the page title and header mention "Kline"
+        # She notices the page title and header mention "Kline"
         self.assertIn('Kline', self.browser.title)
 
-        # He is invited to enter a symbol straight away.
+        # She is invited to enter a symbol straight away.
         inputbox = self.browser.find_element_by_id('id_symbol')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a symbol'
-        )
+            inputbox.get_attribute('placeholder'), 'Enter a symbol')
 
-        # He types "000001.SH" into a text box
+        # She types "000001.SH" into a text box
         sample_symbol = '000001.SH'
         inputbox.send_keys(sample_symbol)
 
         # When she hits enter, the page updates, and now the page display a kline
         # chart with the symbol displayed at the top.
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.fail('Finish the test.')
-
-
-if __name__ == '__main__':
-    unittest.main()
