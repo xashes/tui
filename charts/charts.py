29812763c0352b37b53afdc00c028195884d7c3a
kellerx <keller0319@gmail.com>
2018-04-28 16:50:12 +0800

    display kline and turnover

--- charts/charts.py
+++ charts/charts.py
@@ -1,15 +1,30 @@
-import pyecharts as pe
+from pyecharts import Kline, Bar
+from pyecharts import Grid
 
 
-def kline(bar):
-    data = bar[['open', 'close', 'low', 'high']]
-    kline = pe.Kline(bar['symbol'][0])
+def grids(data):
+    kline = Kline(data.symbol[0])
     kline.add(
-        'day',
+        '',
         data.index,
-        data.values,
+        data.loc[:, ['open', 'close', 'low', 'high']].values,
         mark_line=['max', 'min'],
         mark_line_valuedim=['highest', 'lowest'],
-        bar_width=20,
-        is_datazoom_show=True)
-    return kline
+        is_datazoom_show=True,
+        datazoom_xaxis_index=[0, 1],
+    )
+
+    turnover = Bar()
+    turnover.add(
+        '',
+        data.index,
+        data['turnover'].values / pow(10, 9),
+        mark_line=['max', 'min'],
+        is_datazoom_show=True,
+    )
+
+    grid = Grid(page_title=data['symbol'][0], width=1800, height=900)
+    grid.add(kline, grid_bottom='40%')
+    grid.add(turnover, grid_top='65%')
+    grid.show_config()
+    return grid
--- charts/views.py
+++ charts/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
 from . import charts
-from django.http import HttpResponse
-from django.template import loader
 from tdata import local
 
 REMOTE_HOST = 'https://pyecharts.github.io/assets/js'
@@ -10,11 +8,14 @@ REMOTE_HOST = 'https://pyecharts.github.io/assets/js'
 # Create your views here.
 def kline(request):
     symbol = '000001.SH'
-    data = local.daily(symbol)
-    template = loader.get_template('kline.html')
-    kline = charts.kline(data)
+    bar = local.daily(symbol)
+    grids = charts.grids(bar)
     context = dict(
-        kline=kline.render_embed(),
+        chart=grids.render_embed(),
         host=REMOTE_HOST,
-        script_list=kline.get_js_dependencies())
-    return HttpResponse(template.render(context, request))
+        # chart_id=grids.chart_id,
+        # my_width='100%',
+        # my_height=600,
+        # my_option=json_dumps(grids.options),
+        script_list=grids.get_js_dependencies())
+    return render(request, 'chart.html', context)
